3f9f63a216a4091493492a8880158829c45a1930
Anisa Maharani <anismyes@gmail.com>
2024-04-06 15:35:11 +0800

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcstracking/McsTrackingApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.mcstracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsTrackingApplication.class, args);
+    }
+
+}
